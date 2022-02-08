2cb36d3f24fccced0d26031b7a1e0075965a1cce
Ryan Montgomery <rmontgomery429@gmail.com>
2022-02-08 07:10:30 -0600

    Also cleanup Downloads

--- cleanup.rb
+++ cleanup.rb
@@ -2,8 +2,16 @@
 
 require 'fileutils'
 
-source      = Dir.glob("#{Dir.home}/Desktop/**")
-destination = "#{Dir.home}/Archives/#{Time.now.strftime("%Y-%m-%d")}"
+TODAYS_DATE     = "%Y-%m-%d"
+ARCHIVE_FOLDER = "Archives"
 
-FileUtils.mkdir_p destination
-FileUtils.move source, destination
+def cleanup(folder)
+  source      = Dir.glob(File.join(Dir.home, folder, "**"))
+  destination = File.join(Dir.home, ARCHIVE_FOLDER, Time.now.strftime(TODAYS_DATE), folder)
+
+  FileUtils.mkdir_p destination
+  FileUtils.move source, destination
+end
+
+cleanup("Desktop")
+cleanup("Downloads")
