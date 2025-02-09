f919f92b17e9b38034ae70d49baf8261cf7a8be9
Ryan Montgomery <rmontgomery429@gmail.com>
2025-02-09 16:37:29 -0500

    Add archive compression feature to cleanup script

--- cleanup.rb
+++ cleanup.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-
+require 'date'
 require 'fileutils'
 
 TODAYS_DATE     = "%Y-%m-%d"
@@ -16,3 +16,38 @@ end
 cleanup("Desktop")
 cleanup("Downloads")
 cleanup("Documents")
+
+require 'date'
+require 'fileutils'
+
+def compress_archives!(days_threshold = 30)
+  archive_base = File.join(Dir.home, ARCHIVE_FOLDER)
+  Dir.glob(File.join(archive_base, "*")).sort.each do |archive|
+    folder_name = File.basename(archive)
+    # Attempt to parse the folder name as a date using the defined format.
+    begin
+      archive_date = Date.strptime(folder_name, TODAYS_DATE)
+    rescue ArgumentError
+      puts "Skipping #{archive} (name not in expected date format)"
+      next
+    end
+
+    # Calculate the age in days of the archive folder.
+    age_in_days = (Date.today - archive_date).to_i
+    if age_in_days > days_threshold
+      tar_file = File.join(archive_base, "#{folder_name}.tar.gz")
+      puts "Compressing #{archive} (#{age_in_days} days old) to #{tar_file}..."
+      
+      # Use the tar command to compress the folder.
+      # The -C flag changes directory to the archive_base so that the folder's relative path is preserved.
+      if system("tar -czf #{tar_file} -C #{archive_base} #{folder_name}")
+        FileUtils.rm_rf(archive)
+        puts "Successfully compressed and removed #{archive}"
+      else
+        puts "Failed to compress #{archive}. Check permissions and available space."
+      end
+    end
+  end
+end
+
+compress_archives!
